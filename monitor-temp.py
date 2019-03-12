092f557151e6b6fe931fdf00fab084845556a6b8
Phil Perro <philperro@gmail.com>
2019-03-12 16:03:28 -0400

    convert temp to float

--- monitor-temp.py
+++ monitor-temp.py
@@ -2,9 +2,10 @@ import os
 import time
 
 def measure_temp():
-        temp = os.popen("vcgencmd measure_temp").readline()
-        return (temp.replace("temp=", ""))
+	temp = os.popen("vcgencmd measure_temp").readline()
+	temp = temp.replace("temp=", "").replace("'C\n", "").replace(" ", "")
+	return(float(temp))
 
 while True:
-        print(measure_temp())
-        time.sleep(2)
+	print(measure_temp())
+	time.sleep(2)
